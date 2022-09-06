3f77df05b095fe4be4dfdfcc707e2ea2178ff266
Marcin <marcin.tyman@gmail.com>
2022-09-06 16:09:57 +0200

    Take more lessons.

---
+++ CSharpTutorial/src/ConditionalStms/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpTutorial;
+
+
+public enum AccessLevel
+{
+    READER,
+    DEVELOPER,
+    ADMIN
+}
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var accessLevel = AccessLevel.ADMIN;
+        Console.WriteLine(accessLevel);
+
+        switch(accessLevel)
+        {
+            case AccessLevel.ADMIN:
+            case AccessLevel.DEVELOPER:
+                Console.WriteLine("You can modify the code.");
+                break;
+            case AccessLevel.READER:
+                Console.WriteLine("Reader");
+                break;
+            default:
+                throw new ArgumentException("Access Level Undefined");
+        }
+    }
+}
---
+++ CSharpTutorial/src/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpTutorial;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Provide numbers separated by coma");
+        var inp = Console.ReadLine();
+
+        var numbers = inp.Split(',');
+        int max = int.Parse(numbers[0]);
+        foreach(var num in numbers)
+        {
+            Console.WriteLine(num);
+            if (max < int.Parse(num))
+            {
+                max = int.Parse(num);
+            }
+        }
+        Console.WriteLine(string.Format("Max number: {0}", max));
+    }
+}
