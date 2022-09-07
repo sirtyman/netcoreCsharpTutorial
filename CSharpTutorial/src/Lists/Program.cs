04a58c64047eab9cf0cd4c53dd063a07a02b831b
Marcin <marcin.tyman@gmail.com>
2022-09-07 23:53:09 +0200

    Take another lesson

---
+++ CSharpTutorial/src/Lists/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+var numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+numbers.Add(2);
+numbers.AddRange(new int[] {3, 4, 5, 6, 7 });
+
+while(true)
+{
+    if (!numbers.Remove(3)) break;
+}
+
+foreach (var num in numbers)
+{
+    Console.WriteLine(num);
+}
---
+++ CSharpTutorial/src/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+var stop = false;
+
+do
+{
+    Console.WriteLine("Give names of your friends:");
+    string friends = Console.ReadLine();
+    var friendsList = new List<string>();
+    friendsList.AddRange(friends.Split(";"));
+
+    var count = friendsList.Count;
+    Console.WriteLine(count);
+    switch (count)
+    {
+        case 0:
+            Console.WriteLine("fgdf");
+            stop = true;
+            break;
+        case 1:
+            Console.WriteLine(friendsList.First<string>());
+            break;
+        case 2:
+            Console.WriteLine(friendsList[0] + ", " + friendsList[1]);
+            break;
+        default:
+            Console.WriteLine(
+                friendsList[0] + ", " +
+                friendsList[1] + ", " +
+                friendsList.Last<string>()
+            );
+            break;
+    }
+
+} while (!stop);
+
