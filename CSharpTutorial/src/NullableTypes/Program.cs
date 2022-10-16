2a740de0fa2f3e12ec59c29339ab956fcf3a07b3
sirtyman <marcin.tyman@gmail.com>
2022-10-16 17:21:00 +0200

    Add examples for nullable types and for tuples

---
+++ CSharpTutorial/src/NullableTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Nullable<int> i = null;  // or int?i = null
+
+if (i.HasValue)
+{
+    Console.WriteLine("{}", i);
+}
+else
+{
+    Console.WriteLine("i is null");
+}
---
+++ CSharpTutorial/src/Tuples/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+(int, int, string) myTuple = (3, 4, "Ala");
+
+myTuple = (5, 6, "Marcin");
+
+Console.WriteLine(myTuple.Item3);
+Console.WriteLine(myTuple.ToString());
