b1acbd4afc9297f52e3e0bcc83ad582b92a8d60a
sirtyman <marcin.tyman@gmail.com>
2022-11-04 17:08:13 +0100

    Add theory to xunit tests.

--- CSharpTutorial/src/BankAccount.Test/BankAccountTest.cs
+++ CSharpTutorial/src/BankAccount.Test/BankAccountTest.cs
@@ -10,6 +10,16 @@ public class BankAccountUnitTest
     {   
         BankAccountDll.BankAccount bankAccount = new BankAccountDll.BankAccount();
         bankAccount.Deposit(100.0);
-        Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => bankAccount.Withdrawn(10));
+        Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => bankAccount.Withdrawn(100.01));
+    }
+
+    [Theory]
+    [InlineData(50.0, 40.0, 90.0)]
+    public void Test_Deposit_ExpectedAmount(double deposit1, double deposit2, double expected)
+    {
+        var bankAccount = new BankAccountDll.BankAccount();
+        bankAccount.Deposit(deposit1);
+        bankAccount.Deposit(deposit2);
+        Assert.Equal(bankAccount.Amount, expected);
     }
 }
