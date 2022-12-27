215b358d6f86572f606a4c353c14572e6d229a89
sirtyman <marcin.tyman@gmail.com>
2022-12-27 21:28:41 +0100

    Add simple example with ExpandoObject.

---
+++ CSharpTutorial/src/Expando/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Dynamic;
+using System.Text.Json;
+
+
+namespace DynamicObjects
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            dynamic apiObject = new ExpandoObject();
+            apiObject.name = "Marcin";
+            apiObject.secondName = "Tymon";
+
+            string json = JsonSerializer.Serialize(apiObject);
+            System.Console.WriteLine(json);
+        }
+    }
+
+}
